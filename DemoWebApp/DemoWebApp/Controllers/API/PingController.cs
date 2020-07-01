6451400f324d1551d434003c2d925787d23cc64d
Minh Sang Tran <sangtran.d14ptit@outlook.com>
2020-07-01 08:51:20 +0700

    Thêm api ping pong

---
+++ DemoWebApp/DemoWebApp/Controllers/API/PingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoWebApp.Controllers.API
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public string Pong()
+        {
+            return "pong";
+        }
+    }
+}
